d70fb9a7c9ffd3d1a15b9938490528d4b6e80e04
Ehmad <justEhmadSaeed@gmail.com>
2020-06-06 15:00:38 +0500

    👌 IMPROVE: Rounfd Button Code Refactor

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'login_screen.dart';
 import 'registration_screen.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import '../Components/round_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const id = 'welcome_screen';
@@ -80,34 +81,6 @@ class _WelcomeScreenState extends State<WelcomeScreen>
               title: 'Register',
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class RoundButton extends StatelessWidget {
-  RoundButton({this.color, this.onPressed, this.title});
-
-  final Color color;
-  final Function onPressed;
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: Material(
-        elevation: 5.0,
-        color: color,
-        borderRadius: BorderRadius.circular(30.0),
-        child: MaterialButton(
-          onPressed: onPressed,
-          minWidth: 200.0,
-          height: 42.0,
-          child: Text(
-            title,
-          ),
         ),
       ),
     );
