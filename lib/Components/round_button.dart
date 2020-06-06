854839d2b4f6d73c69f917ad05d4beb6bb4ffcd4
Ehmad <justEhmadSaeed@gmail.com>
2020-06-06 15:00:18 +0500

    📦 NEW: Round Button Widget

---
+++ lib/Components/round_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class RoundButton extends StatelessWidget {
+  RoundButton({this.color, this.onPressed, this.title});
+
+  final Color color;
+  final Function onPressed;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: color,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: onPressed,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            title,
+          ),
+        ),
+      ),
+    );
+  }
+}
