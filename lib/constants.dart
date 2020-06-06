fed4506408af1012f739933993d9678327d91eed
Ehmad <justEhmadSaeed@gmail.com>
2020-06-06 21:38:43 +0500

    👌 IMPROVE: Change the color of hintText

--- lib/constants.dart
+++ lib/constants.dart
@@ -20,7 +20,7 @@ const kMessageContainerDecoration = BoxDecoration(
 
 const kTextFieldDecoration = InputDecoration(
   hintText: 'Enter your email',
-  hintStyle: TextStyle(color: Colors.blue),
+  hintStyle: TextStyle(color: Colors.grey),
   contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
   border: OutlineInputBorder(
     borderRadius: BorderRadius.all(Radius.circular(32.0)),
