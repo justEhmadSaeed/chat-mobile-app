b841b67605d4aa67b4b3e9d2c9d3111acfb256b3
Ehmad <justEhmadSaeed@gmail.com>
2020-06-12 23:16:26 +0500

    🐛 FIX: invalid credentials Exception Handled

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -83,6 +83,9 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                       showSpinner = false;
                     });
                   } catch (e) {
+                    setState(() {
+                      showSpinner = false;
+                    });
                     print(e);
                   }
                 },
